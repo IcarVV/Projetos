d06e8715323c8b1469f307064b196bbf914949e4
IcarVV <icar35773@gmail.com>
2026-01-30 15:52:25 -0300

    Refactor navigation menu: replace old menu with responsive navbar and add toggle functionality

---
+++ site/JS/Nav.js
@@ -0,0 +1,15 @@
+
+    function toggleMenu() {
+
+        document.getElementById ("navLinks") .classList.toggle ("active");
+
+    }
+
+    document.querySelectorAll(".nav-links a").forEach(link => {
+        
+        link.addEventListener("click", () => {
+            
+            document.getElementById("navLinks").classList.remove("active");
+        });
+    });
+
